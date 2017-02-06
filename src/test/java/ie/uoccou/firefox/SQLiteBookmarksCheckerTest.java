d9fe70eacfc7a86bf0ed285a77ce41935a0c7bd8
ultan <ultan@boxer>
2017-02-06 21:28:23 +0000

    shell only

---
+++ src/test/java/ie/uoccou/firefox/SQLiteBookmarksCheckerTest.java
@@ -0,0 +1,67 @@
+package ie.uoccou.firefox;
+
+import org.junit.Test;
+
+public class SQLiteBookmarksCheckerTest {
+
+	@Test
+	public void testMain() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testCheckBookmarks() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetSqlLiteFile() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testSetSqlLiteFile() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetQuery() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testSetQuery() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetCountQuery() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testSetCountQuery() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetDeleteQuery() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testSetDeleteQuery() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetOldest() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testSetOldest() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
